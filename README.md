3d874c84a313fa3ae2c9bf278b2dde4fc1e28871
Jessé Satlei <jessesatlei@hotmail.com>
2020-07-30 07:45:02 -0300

    third day

--- README.md
+++ README.md
@@ -18,17 +18,28 @@ Alterar os vídeos e categorias com conteúdos que você gosta;
 
 Aprender sobre as páginas no estilo SPA, as vantagens do State e criação de um carrossel para os vídeos
 
+### Desafio
+
+Colocar algo legal na página 404;
+
 ## Dia 3
 **Formulários reutilizáveis e mais componentes:**
 
 Explorar a fundo a criação de componentes, reutilizar lógicas comuns com hooks e criar campos de formulários animados
+
+### Desafio
+Fazer o textarea funcionar
+Fazer a animação da netflix nos campos do formulário
+fazer os estilos
+Criar uma página para fazer cadastro de pokémons e trazer seus pokémons favoritos dentro dos valores iniciais (ao invés de categorias)
+Tentar criar um card para mostrar isso com styled components
 
 ## Dia 4
 **AJAX e Validação:**
 
 Entender o protocolo HTTP, aprender como enviar e buscar dados de um back-end, adicionar suporte de validação em nossos campos e colocar a cópia do seu projeto no ar
 
-##Dia 5
+## Dia 5
 **Formulário de cadastro e remoção de vídeos:**
 
 Finalizar o cadastro de vídeos, configurar remoções e conhecer melhor o mercado de React
@@ -46,3 +57,5 @@ Para preparar o projeto para deploy utilize o seguinte comando:
 Styled Components - Biblioteca React para facilitação da criação de componentes - https://styled-components.com/
 React Router - Biblioteca React para facilitação do routeamento da aplicação - https://reactrouter.com/web/guides/quick-start
 React Slick - Biblioteca React para criação de Carousel - https://react-slick.neostack.com/
+Fonts Google - Lugar onde pode pegar fonte diretamente desenvolvidas pelo Google - https://fonts.google.com/
+Fontmeme - Site onde pode criar sua propria logo - https://fontmeme.com/netflix-font/
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,23 +1,115 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
+import FormField from '../../../components/FormField';
 
 function CadastroCategoria() {
+  // Está sendo desistruturado o objeto colocando o [] antes do =, se trabalha com três valores abaixo, nome, a função que altera o nome e o valor padrão.
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: ''
+  }
+  const [categorias, setCategorias] = useState([]);
+  const [values, setValues] = useState(valoresIniciais);
+
+  function setValue(chave, valor) {
+    setValues({
+      ...values,
+      [chave]: valor,
+    });
+  }
+
+  function handleChange(infoEvento) {
+    //const { getAttribute, value } = infoEvento.target;
+    setValue(
+      infoEvento.target.getAttribute('name'),
+      infoEvento.target.value
+    );
+  }
+
   return (
     <PageDefault>
-      <h1>Cadastrar Categoria</h1>
+      <h1>Cadastrar Categoria: {values.nome}</h1>
 
-      <form>
+      <form onSubmit={function handleSubmit(infoEvento) {
+        infoEvento.preventDefault();
+        setCategorias([
+          ...categorias,
+          values
+        ]);
 
-        <label>
-          Nome da Categoria:
-          <input  type="text" />
-        </label>
-
+        setValues(valoresIniciais); //utilizado para toda vez que cadastrar uma categoria, limpar os campos
+      }}>
+        <FormField
+          label="Nome da Categoria: "
+          type="text"
+          name="nome"
+          value={values.nome}
+          onChange={handleChange}
+        />
+        <FormField
+          label="Descrição: "
+          type="text"
+          name="descricao"
+          value={values.descricao}
+          onChange={handleChange}
+        />
+        <FormField
+          label="Cor: "
+          type="color"
+          name="cor"
+          value={values.cor}
+          onChange={handleChange}
+        />
+        {/* <div>
+          <label>
+            Nome da Categoria:
+            <input
+              type="text"
+              name="nome"
+              value={values.nome}
+              onChange={handleChange}
+            />
+          </label>
+        </div> */}
+        {/* <div>
+          <label>
+            Descrição:
+            <textarea
+              type="text"
+              name="descricao"
+              value={values.descricao}
+              onChange={handleChange}
+            />
+          </label>
+        </div> */}
+        {/* <div>
+          <label>
+            Cor:
+            <input
+              type="color"
+              name="cor"
+              value={values.cor}
+              onChange={handleChange}
+            />
+          </label>
+        </div> */}
         <button>
           Cadastrar
         </button>
       </form>
+
+      <ul>
+        {categorias.map((categoria, indice) => {
+          return (
+            <li key={`${categoria}${indice}`}>
+              {categoria.nome}
+            </li>
+          )
+        })}
+      </ul>
+
       <Link to="/">
         Ir para a Home
       </Link>
